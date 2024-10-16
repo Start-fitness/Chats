beb23d7108bd6cdd854aaad676e58c4a838c2aa2
Andrei Gritsevich <grand16121980@yandex.ru>
2024-10-16 12:24:46 +0300

    Add config module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,18 +5,26 @@ import { UserModule } from './modules/user.module';
 import { ChatModule } from './modules/chat.module';
 import { SubscriptionModule } from './modules/subscription.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [UserModule, ChatModule, SubscriptionModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: +process.env.POSTGRES_PORT || 5432,
-      username: process.env.POSTGRES_USER || 'user',
-      password: process.env.POSTGRES_PASSWORD || 'password',
-      database: process.env.POSTGRES_DB || 'app_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
